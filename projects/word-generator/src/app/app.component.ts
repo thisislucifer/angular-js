05bb4def1ff344f4fe00016ae952771743043b87
aayush-scalex <aayush@scalextechnologies.com>
2020-10-04 15:11:35 +0530

    word-generatator: added logic to generate words

--- projects/word-generator/src/app/app.component.ts
+++ projects/word-generator/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import arrayWords from '../utils/words'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words = ''
+  limit = 10;
+  
+  handleSliderChange(newLimit : number){
+    this.limit = newLimit; //We have updated the value of limit from 10 to new-value;
+  }
+
+  generate(){
+    this.words = arrayWords.slice(0, this.limit).join(' ');
+  }
 }
