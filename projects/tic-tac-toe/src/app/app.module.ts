dce62475887799f735dc731911c0911e2e9e8bc5
aayush-scalex <aayush@scalextechnologies.com>
2020-10-11 14:57:20 +0530

    tic-tak-toe : gen new component add inject child component into parent

--- projects/tic-tac-toe/src/app/app.module.ts
+++ projects/tic-tac-toe/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent,
   ],
   imports: [
     BrowserModule,
