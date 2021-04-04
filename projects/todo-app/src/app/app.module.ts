860640baf7df53b9b90bfd030be46a2772dee06d
Aayush Dalvi <aayushdalvi100@gmail.com>
2021-04-04 22:50:37 +0530

    todo-app : initialize project, add header, add model, update readme

---
+++ projects/todo-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { HeaderComponent } from './layout/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
